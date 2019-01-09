899e75fc7b313dcc45608b4b4ea01d749e551d23
Imelda Izaguirre <imeldaizaguirre@gmail.com>
2019-01-09 05:31:15 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,6 +6,6 @@ function updatePlaylist(obj,key,value){
   return obj[key] = value
 }
 
-function removeFromPlaylist(obj,name){
-  delete obj.name; 
+function removeFromPlaylist(obj,artistName){
+  delete obj.artistName; 
 }
